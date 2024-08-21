2fa0e05aed3799979d5734785832ede8387ee62e
alessandroTola <alessandro.tola@horsa.it>
2024-08-21 09:40:45 +0200

    chore(api): add functions interfaces

--- src/file-parser/file-parser.controller.ts
+++ src/file-parser/file-parser.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Logger, Query } from '@nestjs/common';
 import { FileParserService } from './file-parser.service';
+import { promises } from 'dns';
 
 @Controller('file-parser')
 export class FileParserController {
-  constructor(private readonly fileParserService: FileParserService) {}
+  constructor(private readonly fileParserService: FileParserService) { }
+
+  /**
+   * End point to parse a file
+   * @param path File path
+   */
+  @Get('parse-file')
+  async parseFile(@Query('path') path: string): Promise<any> {
+    Logger.log(`FileParserController.parseFile.begin: path=${path}`);
+    return this.fileParserService.parseFile(path);
+  }
 }
--- src/file-parser/file-parser.service.ts
+++ src/file-parser/file-parser.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
-export class FileParserService {}
+export class FileParserService {
+  async parseFile(path: string) {
+    Logger.log(`FileParserService.parseFile.begin: path=${path}`);
+
+  }
+
+  /**
+   * Function to get file content based on file path,
+   * if file path contains 'http' or 'https' then it will fetch content from URL
+   * otherwise it will read content from file
+   * @param path 
+   * @returns 
+   */
+  private async getFileContent(path: string): Promise<string> {
+    // Implementation to read file content
+    Logger.log(`FileParserService.getFileContent.begin`);
+    return 'file-content';
+  }
+
+  /**
+   * Function to count words into a file content
+   * @param content 
+   * @returns 
+   */
+  private wordsCount(content: string): number {
+    // Implementation to count words
+    Logger.log(`FileParserService.wordsCount.begin`);
+    return 0;
+  }
+
+  /**
+   * Function to count letters into a file content
+   * @param content 
+   * @returns 
+   */
+  private lettersCount(content: string): number {
+    // Implementation to count letters
+    Logger.log(`FileParserService.lettersCount.begin`);
+    return 0;
+  }
+
+  /**
+   * Function to count spaces into a file content
+   * @param content 
+   * @returns 
+   */
+  private spacesCount(content: string): number {
+    // Implementation to count spaces
+    Logger.log(`FileParserService.spacesCount.begin`);
+    return 0;
+  }
+
+
+  /**
+   * Method to count wards that count is greater then 10
+   */
+  private wordsCountGreater(content: string): { [key: string]: number } {
+    // Implementation to count words
+    return {};
+  }
+}
